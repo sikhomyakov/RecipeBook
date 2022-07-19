402cfafd94e13db5c0c8fa85e27dc8962a40d71e
sikhomyakov@gmail.com <Supernova2022>
2022-07-19 16:43:14 +0500

    Version 6.1 RepositoryImpl

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun get(): LiveData<Post>
-    fun like()
-    fun share()
+    val data: LiveData<List<Post>>
+    fun like(postId: Long)
+    fun share(postId: Long)
     fun counter(count: Int): String
 }
--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -1,36 +1,50 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.netology.nmedia.dto.*
 
 class PostRepositoryInMemoryImpl : PostRepository {
-    private var post = Post(
-        id = 1,
-        author = "Нетология. Университет интернет-профессий будущего",
-        content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-        published = "21 мая в 18:36",
-        likedByMe = false,
-        likes = 999,
-        shares = 993,
-        sharedByMe = false,
-        views = 5,
-        viewedBy = false,
-    )
-    private val data = MutableLiveData(post)
 
-    override fun get(): LiveData<Post> = data
-    override fun like() {
-        post = post.copy(
-            likedByMe = !post.likedByMe,
-            likes = if (!post.likedByMe) ++post.likes else --post.likes
-        )
-        data.value = post
+    private val posts
+        get() = checkNotNull(data.value) {
+
+        }
+
+    override val data = MutableLiveData(
+        List(10) { index ->
+            Post(
+                id = index + 1L,
+                author = "Нетология. Университет интернет-профессий будущего",
+                content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+                published = "21 мая в 18:36",
+                likedByMe = false,
+                likes = 999,
+                shares = 993,
+                sharedByMe = false,
+                views = 5,
+                viewedBy = false,
+            )
+        }
+    )
+
+    override fun like(postId: Long) {
+        data.value = posts.map {
+            if (it.id != postId) it
+            else
+                it.copy(
+                    likedByMe = !it.likedByMe
+                )
+        }
     }
 
-    override fun share() {
-        ++post.shares
-        data.value = post
+    override fun share(postId: Long) {
+        data.value = posts.map {
+            if (it.id != postId) {
+                it
+            } else {
+                it.copy(shares = it.shares + 1)
+            }
+        }
     }
 
     override fun counter(count: Int): String {
@@ -40,4 +54,5 @@ class PostRepositoryInMemoryImpl : PostRepository {
             (count in 10_000..999_999) -> "${count / 1000}K"
             else -> count
         }.toString()
-    }}
+    }
+}
